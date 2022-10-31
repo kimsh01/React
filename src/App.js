645190bae6c0d7921fe1ccc2baa73a61063e462c
kimsh01 <rlatjsgur01@gmail.com>
2022-10-31 15:50:59 +0900

    shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import data from './data.js';
+import {Routes, Route, Link, useNavigate, Outlet} from "react-router-dom";
+import Detail from './Detail.js';
 function App() {
+
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+      <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="me-auto">
+              <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+              <Nav.Link onClick={()=>{navigate('./Detail')}}>Cart</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+
+
+    <Routes>
+      <Route path="/" element={
+        <>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+              {
+                shoes.map((a, i)=>{
+                  return(
+                    <Card shoes={shoes[i]} i={i+1}></Card>
+                  )
+                })
+              }        
+                </div>
+            </div>
+        </>    
+      } />
+        <Route path="/detail" element={<Detail/>} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+
+ 
+
+      
     </div>
   );
+
+function Card(props){
+  return(
+      <div className="col-md-4">
+        <img src={'https://codingapple1.github.io/shop/shoes'+ props.i +'.jpg'} width="80%"/>
+        <h4>{ props.shoes.title }</h4>
+        <p>{ props.shoes.price }</p>
+      </div>
+  )
+}
+
+
 }
 
 export default App;
